5f624402868f996d6cbb63c051bb4e05c3a81fd1
Chris Balow <cbalow@edmentum.com>
2020-03-19 08:59:10 -0400

    start of module 3

--- starting/TheCodeCamp/App_Start/AutofacConfig.cs
+++ starting/TheCodeCamp/App_Start/AutofacConfig.cs
@@ -11,28 +11,28 @@ using TheCodeCamp.Data;
 
 namespace TheCodeCamp
 {
-  public class AutofacConfig
-  {
-    public static void Register()
+    public class AutofacConfig
     {
-      var bldr = new ContainerBuilder();
-      var config = GlobalConfiguration.Configuration;
-      bldr.RegisterApiControllers(Assembly.GetExecutingAssembly());
-      RegisterServices(bldr);
-      bldr.RegisterWebApiFilterProvider(config);
-      bldr.RegisterWebApiModelBinderProvider();
-      var container = bldr.Build();
-      config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
-    }
+        public static void Register()
+        {
+            var bldr = new ContainerBuilder();
+            var config = GlobalConfiguration.Configuration;
+            bldr.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            RegisterServices(bldr);
+            bldr.RegisterWebApiFilterProvider(config);
+            bldr.RegisterWebApiModelBinderProvider();
+            var container = bldr.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
 
-    private static void RegisterServices(ContainerBuilder bldr)
-    {
-      bldr.RegisterType<CampContext>()
-        .InstancePerRequest();
+        private static void RegisterServices(ContainerBuilder bldr)
+        {
+            bldr.RegisterType<CampContext>()
+                .InstancePerRequest();
 
-      bldr.RegisterType<CampRepository>()
-        .As<ICampRepository>()
-        .InstancePerRequest();
+            bldr.RegisterType<CampRepository>()
+                .As<ICampRepository>()
+                .InstancePerRequest();
+        }
     }
-  }
 }
--- starting/TheCodeCamp/App_Start/WebApiConfig.cs
+++ starting/TheCodeCamp/App_Start/WebApiConfig.cs
@@ -5,21 +5,21 @@ using System.Web.Http;
 
 namespace TheCodeCamp
 {
-  public static class WebApiConfig
-  {
-    public static void Register(HttpConfiguration config)
+    public static class WebApiConfig
     {
-      // Web API configuration and services
-      AutofacConfig.Register();
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            AutofacConfig.Register();
 
-      // Web API routes
-      config.MapHttpAttributeRoutes();
+            // Web API routes
+            config.MapHttpAttributeRoutes();
 
-      config.Routes.MapHttpRoute(
-          name: "DefaultApi",
-          routeTemplate: "api/{controller}/{id}",
-          defaults: new { id = RouteParameter.Optional }
-      );
+            config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+            );
+        }
     }
-  }
 }
